7f1f5b296d451aea934b1077658c8ad18a6f9cde
Brian Qiu <brianqiu2000@gmail.com>
2017-01-14 22:29:48 -0800

    added the jinja2

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import webapp2
+import jinja2
+import os
+
+template_env=jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write("Welcome")
+        template = template_env.get_template('main.html')
+        self.response.out.write(template.render())
 
 
 
