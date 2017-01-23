aabb905176345c608c45a80b98c6549abd012cf6
Brian Qiu <brianqiu2000@gmail.com>
2017-01-22 19:58:55 -0800

    ronin placeholder

---
+++ js/ronin.js
@@ -0,0 +1,7 @@
+class ronin {
+    constructor(katana, endurance, honor) { 
+            this.katana = katana;
+            this.endurance = endurance;
+            this.honor = honor;
+        }
+    }
