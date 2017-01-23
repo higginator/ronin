0c102e9bb4f6743e714b714936fac7ee66eaf614
Ryan Higgins <ryanhigs@gmail.com>
2017-01-22 20:04:51 -0800

    capitalized class name

--- js/ronin.js
+++ js/ronin.js
@@ -1,5 +1,5 @@
-class ronin {
-    constructor(katana, endurance, honor) { 
+class Ronin {
+    constructor(katana, endurance, honor) {
             this.katana = katana;
             this.endurance = endurance;
             this.honor = honor;
